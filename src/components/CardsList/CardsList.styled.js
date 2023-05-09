67f0f30a0a2edb175be671b11bcc61d15cea8a48
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-05-09 22:57:57 +0300

    change: cards position

--- src/components/CardsList/CardsList.styled.js
+++ src/components/CardsList/CardsList.styled.js
@@ -1,9 +1,9 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
 
 export const TweetsList = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   gap: 20px;
 `;
