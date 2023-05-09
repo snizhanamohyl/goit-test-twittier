9c93f8536af7435a1b777318c8473944ba2de838
snizhanamohyl <110724174+snizhanamohyl@users.noreply.github.com>
2023-05-09 15:59:52 +0000

    Deploying to gh-pages from @ snizhanamohyl/goit-test-twittier@59f74d0ec647293ae67caf34f6e23a5e8a09cf88 🚀

---
+++ static/js/504.755dbe2f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[504],{504:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(791),u=n(184),a=(0,r.lazy)((function(){return Promise.all([n.e(279),n.e(526)]).then(n.bind(n,405))}));function c(){return(0,u.jsx)(u.Fragment,{children:(0,u.jsx)(a,{})})}}}]);
+//# sourceMappingURL=504.755dbe2f.chunk.js.map
