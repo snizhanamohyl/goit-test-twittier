ad8bdb8d756b5e3479665b82c196a3d14919cabf
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-05-09 18:54:13 +0300

    add: lazy loading for TweetsPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import TweetsPage from "pages/TweetsPage/TweetsPage";
+// import TweetsPage from "pages/TweetsPage/TweetsPage";
 import HomePage from "pages/HomePage/HomePage";
 import Layout from "components/Layout/Layout";
+import { lazy } from "react";
+
+const TweetsPage = lazy(() => import('pages/TweetsPage/TweetsPage'));
+console.log('lazy')
 
 function App() {
 
