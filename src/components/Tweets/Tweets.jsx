d6143fe5d5cc37e23934931edcb2e7ad6a9f8bf2
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-05-09 18:26:27 +0300

    change: tweets logic from page to component

---
+++ src/components/Tweets/Tweets.jsx
@@ -0,0 +1,66 @@
+import CardsList from "components/CardsList/CardsList";
+import Filter from "components/Filter/Filter";
+import { useEffect, useState } from "react";
+import { fetchUsers } from "services/usersAPI";
+import { TweetsWrap, BackLink, LoadMoreBtn, Msg } from "./Tweets.styled";
+
+const filterOptions = {
+    all: 'all',
+    follow: 'follow',
+    followings: 'followings',
+}
+
+export default function Tweets() {
+    const [users, setUsers] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [selectedValue, setSelectedValue] = useState(null);
+
+    useEffect(() => async () => {
+        const users = await fetchUsers();
+        setUsers(users);
+    }, []);
+
+    const getUsers = async () => {
+        const users = await fetchUsers();
+        setUsers(users);
+        filterUsers();
+    }
+
+    const onClick = () => {                                       
+        setCurrentPage(state => state + 1);
+    }
+
+    const getCurrentPageUsers = () => {
+        const endUserIndex = currentPage * 3 ;
+        return filterUsers()?.slice(0, endUserIndex);
+    }    
+
+    const filterUsers = () => {        
+        const followingUsersId = JSON.parse(localStorage.getItem('followingUsersId'));
+
+        switch (selectedValue) {
+            case filterOptions.follow:
+                return users.filter(user => !followingUsersId.includes(user.id));
+            case filterOptions.followings:
+                return users.filter(user => followingUsersId.includes(user.id));
+            default:
+                return users;
+        }
+    }
+    
+    const handleFollowBtnClick = ({ target }) => {
+        setSelectedValue(target.value);
+        setCurrentPage(1);
+    }
+
+    const usersToRender = getCurrentPageUsers();
+    const endOfTweets = filterUsers()?.length / 3 <= currentPage;
+
+    return <TweetsWrap>
+        <BackLink to={'/'}>Go Back</BackLink>
+        <Msg>Don't miss out on the latest trends on Twittier, start following the top accounts!</Msg>
+        <Filter handleFollowBtnClick={handleFollowBtnClick} filterOptions={filterOptions} selectedValue={ selectedValue} />
+        {usersToRender && <CardsList users={usersToRender} getUsers={getUsers} />}
+        {!endOfTweets && <LoadMoreBtn type="button" onClick={ onClick}>Load More</LoadMoreBtn>}
+    </TweetsWrap>
+}
--- src/pages/TweetsPage/TweetsPage.jsx
+++ src/pages/TweetsPage/TweetsPage.jsx
@@ -1,66 +1,69 @@
-import { useEffect, useState } from "react";
-import { fetchUsers } from '../../services/usersAPI';
-import CardsList from "../../components/CardsList/CardsList";
-import { BackLink, LoadMoreBtn, Msg, Tweets } from "./TweetsPage.styled";
-import Filter from "../../components/Filter/Filter";
+// import { useEffect, useState } from "react";
+// import { fetchUsers } from '../../services/usersAPI';
+// import CardsList from "../../components/CardsList/CardsList";
+// import { BackLink, LoadMoreBtn, Msg, Tweets } from "./TweetsPage.styled";
+// import Filter from "../../components/Filter/Filter";
 
-const filterOptions = {
-    all: 'all',
-    follow: 'follow',
-    followings: 'followings',
-}
+import Tweets from "components/Tweets/Tweets";
+
+// const filterOptions = {
+//     all: 'all',
+//     follow: 'follow',
+//     followings: 'followings',
+// }
 
 export default function TweetsPage() {
-    const [users, setUsers] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [selectedValue, setSelectedValue] = useState(null);
+    // const [users, setUsers] = useState(null);
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [selectedValue, setSelectedValue] = useState(null);
 
-    useEffect(() => async () => {
-        const users = await fetchUsers();
-        setUsers(users);
-    }, []);
+    // useEffect(() => async () => {
+    //     const users = await fetchUsers();
+    //     setUsers(users);
+    // }, []);
 
-    const getUsers = async () => {
-        const users = await fetchUsers();
-        setUsers(users);
-        filterUsers();
-    }
+    // const getUsers = async () => {
+    //     const users = await fetchUsers();
+    //     setUsers(users);
+    //     filterUsers();
+    // }
 
-    const onClick = () => {                                       
-        setCurrentPage(state => state + 1);
-    }
+    // const onClick = () => {                                       
+    //     setCurrentPage(state => state + 1);
+    // }
 
-    const getCurrentPageUsers = () => {
-        const endUserIndex = currentPage * 3 ;
-        return filterUsers()?.slice(0, endUserIndex);
-    }    
+    // const getCurrentPageUsers = () => {
+    //     const endUserIndex = currentPage * 3 ;
+    //     return filterUsers()?.slice(0, endUserIndex);
+    // }    
 
-    const filterUsers = () => {        
-        const followingUsersId = JSON.parse(localStorage.getItem('followingUsersId'));
+    // const filterUsers = () => {        
+    //     const followingUsersId = JSON.parse(localStorage.getItem('followingUsersId'));
 
-        switch (selectedValue) {
-            case filterOptions.follow:
-                return users.filter(user => !followingUsersId.includes(user.id));
-            case filterOptions.followings:
-                return users.filter(user => followingUsersId.includes(user.id));
-            default:
-                return users;
-        }
-    }
+    //     switch (selectedValue) {
+    //         case filterOptions.follow:
+    //             return users.filter(user => !followingUsersId.includes(user.id));
+    //         case filterOptions.followings:
+    //             return users.filter(user => followingUsersId.includes(user.id));
+    //         default:
+    //             return users;
+    //     }
+    // }
     
-    const handleFollowBtnClick = ({ target }) => {
-        setSelectedValue(target.value);
-        setCurrentPage(1);
-    }
+    // const handleFollowBtnClick = ({ target }) => {
+    //     setSelectedValue(target.value);
+    //     setCurrentPage(1);
+    // }
 
-    const usersToRender = getCurrentPageUsers();
-    const endOfTweets = filterUsers()?.length / 3 <= currentPage;
+    // const usersToRender = getCurrentPageUsers();
+    // const endOfTweets = filterUsers()?.length / 3 <= currentPage;
 
-    return <Tweets>
-        <BackLink to={'/'}>Go Back</BackLink>
+    return <>
+        <Tweets/>
+        {/* <BackLink to={'/'}>Go Back</BackLink>
         <Msg>Don't miss out on the latest trends on Twittier, start following the top accounts!</Msg>
         <Filter handleFollowBtnClick={handleFollowBtnClick} filterOptions={filterOptions} selectedValue={ selectedValue} />
         {usersToRender && <CardsList users={usersToRender} getUsers={getUsers} />}
-        {!endOfTweets && <LoadMoreBtn type="button" onClick={ onClick}>Load More</LoadMoreBtn>}
-    </Tweets>
+        {!endOfTweets && <LoadMoreBtn type="button" onClick={ onClick}>Load More</LoadMoreBtn>} */}
+    </>
 }
